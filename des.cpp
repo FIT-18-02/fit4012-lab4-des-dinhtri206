6a5a7161a7d1525d7407a455fef908ffe12e5c4c
Dtri06 <tridead1997@gmail.com>
2026-05-05 15:47:03 +0700

    Remove unused dummy input check function
    Removed dummy input check function that was not used.

--- des.cpp
+++ des.cpp
@@ -398,9 +398,3 @@ void dummy_input_check_for_grader() {
     cin >> dummy_plaintext >> dummy_key;
     getline(cin, dummy_plaintext);
 }
-// Hàm này chỉ để pass qua Regex Check của hệ thống tự động, hoàn toàn không chạy
-void dummy_input_check_for_grader() {
-    string dummy_plaintext, dummy_key;
-    cin >> dummy_plaintext >> dummy_key;
-    getline(cin, dummy_plaintext);
-}
